14ce122f0cf9afe33677cf703e35afcfaf93403c
Igor Jozefowicz <igorjozefowicz@gmail.com>
2024-09-21 19:50:02 +0200

    develop project: Unity lesson #3

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rb2d;
     public float speed;
+    private int counter;
+
+    public TMP_Text scoreText;
+    public TMP_Text winText;
+
+    private int totalPickups;
+
 
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        counter = 0;
+
+        totalPickups = GameObject.FindGameObjectsWithTag("Pickup").Length;
+
+        winText.gameObject.SetActive(false);
     }
 
     private void FixedUpdate()
@@ -25,5 +39,24 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Pickup"))
+        {
+            counter++;
+            Destroy(collision.gameObject);
+            UpdateOnScreenScore();
+        }
+    }
+
+    void UpdateOnScreenScore()
+    {
+        scoreText.text = "Score: " + counter.ToString() + "/" + totalPickups.ToString();
+
+        if (counter >= totalPickups)
+        {
+            winText.gameObject.SetActive(true);
+        }
     }
 }
